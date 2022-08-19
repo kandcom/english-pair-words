c4c97ff12dd5fb657a8d6b6493689e16bbddcaef
kandcom <korkinav8@gmail.com>
2022-08-20 00:08:08 +0600

    finished application on video "flutter crash course"

---
+++ lib/main.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import './random_words.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      theme: ThemeData(
+        primaryColor: Color.fromARGB(255, 190, 138, 199),
+      ),
+      home: RandomWords(),
+    );
+  }
+}
